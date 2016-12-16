c8d7a4adf84f5bf579a1836869e9c0267c7814ff
mooow <mureulor@gmail.com>
2016-12-16 01:41:17 +0100

    Minor changes, testing
    * Added license on each source file
    * fixed syntax on source files
    * minor stylistic changes

--- alfieri.zsh
+++ alfieri.zsh
@@ -1,6 +1,30 @@
-CONFIGURATION_FILE = "alfieri.zsh"
-source "${CONFIGURATION_FILE}"
-ALFIERI_STAGES = "${ALFIERI_DIR}/stages"
+#!/usr/bin/zsh
+source "/opt/alfieri/alfieri.cfg"   # Load configuration file
+
+# This file is part of alfieri
+# Alfieri is  Copyright (c) 2016 Lorenzo Mureu <mureulor@gmail.com>
+# 
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+# 
+# The above copyright notice and this permission notice shall be included in all
+# copies or substantial portions of the Software.
+# 
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+# SOFTWARE.
+
+autoload -Uz colors && colors
+
+ALFIERI_STAGES="${ALFIERI_DIR}/stages"
 
 loadstages() {
     for stage in "$@"
@@ -11,13 +35,15 @@ loadstages() {
 }
 
 stage() {
+    echo -n "$fg_bold[yellow]"
     printf "Stage %3d: " $STAGE
+    echo -n "$reset_color"
     echo "$@"
     STAGE=$[$STAGE + 1]
 }
 
 echorun() {
-    echo "$@"
+    echo "$fg_color[blue]$@$reset_color"
     "$@"
 }
 
@@ -31,19 +57,23 @@ die() {
 }
 
 _continue() {
-    echo -n Do you want to continue? [y/N]
+    echo -n "$fg_color[red]"
+    echo -n "Do you want to continue? [y/N]"
+    echo -n "$reset_color"
     read -r cont
     [[ "$cont" != "Y" && "$cont" != "y" ]] && exit 0
 }
 
 usage() {
-    cat << EOF Syntax: $0 [stage]
+    cat << EOF
+Syntax: $0 [stage]
 If stage is omitted, 00 (double zero) is assumed.
 Possible values for stage:
     000: pre-installation   (setup)
-    100: installation       (pacstrap)
-    200: installation       (from chroot)
-    300: post-installation
+    100: installation       (before chroot)
+    200: installation       (inside chroot)
+    300: installation       (after chroot)
+    400: post-installation  (after reboot)
 EOF
 }
 
@@ -52,7 +82,7 @@ alias ::='echorunno'
 
 [ $# -gt 1 ] && usage       # Syntax: $0 
 opt=$1                      # If an arg is provided, that is used.
-[ $# -eq 0 ] && opt="00"    # Otherwise 00 is assumed
+[ $# -eq 0 ] && opt="000"   # Otherwise 00 is assumed
 case $opt in
 000)
     source "${ALFIERI_DIR}/pre-installation.zsh"
--- installation.zsh
+++ installation.zsh
@@ -1,26 +1,47 @@
-STAGE=100
-stage Installation
+# This file is part of alfieri
+# Alfieri is  Copyright (c) 2016 Lorenzo Mureu <mureulor@gmail.com>
+# 
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+# 
+# The above copyright notice and this permission notice shall be included in all
+# copies or substantial portions of the Software.
+# 
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+# SOFTWARE.
 
-stage Installing base packages
+STAGE=100
+stage "Installation"
+
+stage "Installing base packages"
 : pacstrap /mnt base base-devel
 
-stage Generate fstab
+stage "Generate fstab"
 : genfstab -p -U /mnt >> /mnt/etc/fstab
 
-stage Copy alfieri into target system
+stage "Copy alfieri into target system"
 : mkdir -p ${TARGET_ALFIERI_DIR}
 : cp -r "${ALFIERI_DIR}" /mnt/${TARGET_ALFIERI_DIR}
 
 laodstages ${ALFIERI_STAGES}/1*.stage
 
-stage Chrooting into target system
+stage "Chrooting into target system"
 : arch-chroot /mnt ${TARGET_ALFIERI_DIR}/${ALFIERI_BIN} ${TARGET_STAGE}
 
 STAGE=300
 
-stage Loading after-chroot external stages
+stage "Loading after-chroot external stages"
 loadstages ${ALFIERI_STAGES}/3*.stage
 
-stage Reboot
+stage "Reboot"
 : umount -R /mnt
 : reboot
